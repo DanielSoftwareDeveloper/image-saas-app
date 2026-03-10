800f6f6325fa069c4b0b0931b38c56ce8ffdb363
Daniel Reyes <danielsoftwaredev@gmail.com>
2026-03-09 23:50:53 -0600

    07: aws-modal

--- web-app/src/env.ts
+++ web-app/src/env.ts
@@ -5,7 +5,7 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
     BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z.string().url().min(1).optional(),
+    BETTER_AUTH_URL: z.url().min(1).optional(),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
@@ -14,6 +14,8 @@ export const env = createEnv({
     AWS_ACCESS_KEY_ID: z.string().min(1),
     AWS_SECRET_ACCESS_KEY: z.string().min(1),
     AWS_REGION: z.string().min(1),
+    AWS_S3_BUCKET_NAME: z.string().min(1),
+    MODAL_ZIMAGE_URL: z.url().min(1),
   },
   client: {},
   experimental__runtimeEnv: {},
